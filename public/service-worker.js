27d4aacfc66efad257d5e4c8e923f22db93b3b30
WD40 <69653174+duncangw1@users.noreply.github.com>
2021-01-21 16:58:10 -0500

    Created db.js for IndexedDB and updated connection in index.html and service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,9 +2,9 @@ console.log("Hello from your service worker!");
 
 const FILES_TO_CACHE = [
   "/",
-  "/index.html",
   "/styles.css",
   "/index.js",
+  "/db.js",
   "/manifest.webmanifest",
   "/icons/icon-192x192.png",
   "/icons/icon-512x512.png",
